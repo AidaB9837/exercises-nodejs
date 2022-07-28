50c7a89a089b093ae7c78ce0deccfbd267c57bc0
Aida <aidabrugnone9@gmail.com>
2022-07-28 19:52:34 +0200

    feat: Inspect an HTTP Response with Curl

---
+++ 08-InspectAnHTTPResponseWithCurl/response-headers.js
@@ -0,0 +1,68 @@
+// This JS file was created from the command-line by running the '--output' command
+
+// As you can see in line 8, the value of the content-type reponse header is: "application/json; charset=uft-8"
+
+/*
+HTTP/2 200 
+date: Thu, 28 Jul 2022 17:44:18 GMT
+content-type: application/json; charset=utf-8
+x-powered-by: Express
+x-ratelimit-limit: 1000
+x-ratelimit-remaining: 999
+x-ratelimit-reset: 1655578284
+vary: Origin, Accept-Encoding
+access-control-allow-credentials: true
+cache-control: max-age=43200
+pragma: no-cache
+expires: -1
+x-content-type-options: nosniff
+etag: W/"5e6-4bSPS5tq8F8ZDeFJULWh6upjp7U"
+via: 1.1 vegur
+cf-cache-status: HIT
+age: 6458
+expect-ct: max-age=604800, report-uri="https://report-uri.cloudflare.com/cdn-cgi/beacon/expect-ct"
+report-to: {"endpoints":[{"url":"https:\/\/a.nel.cloudflare.com\/report\/v3?s=7ZyNuRRq8QlTs0%2FF%2FhwNUCpvF4Ly2ld9D2OXaK%2FW1prJEb3Besk0ryhDboShmvkgIePC4GC4ma%2FDpNK3B8L590KnP%2BkMnRZ1%2FgkK3xcdbYAFKa8YP52nx91E6kGAFBjBs8MKyP3W2wQgVpixks9%2F"}],"group":"cf-nel","max_age":604800}
+nel: {"success_fraction":0,"report_to":"cf-nel","max_age":604800}
+server: cloudflare
+cf-ray: 731f6c0cf8a03751-MXP
+alt-svc: h3=":443"; ma=86400, h3-29=":443"; ma=86400
+*/
+
+// This is the Body response
+[
+  {
+    "postId": 1,
+    "id": 1,
+    "name": "id labore ex et quam laborum",
+    "email": "Eliseo@gardner.biz",
+    "body": "laudantium enim quasi est quidem magnam voluptate ipsam eos\ntempora quo necessitatibus\ndolor quam autem quasi\nreiciendis et nam sapiente accusantium"
+  },
+  {
+    "postId": 1,
+    "id": 2,
+    "name": "quo vero reiciendis velit similique earum",
+    "email": "Jayne_Kuhic@sydney.com",
+    "body": "est natus enim nihil est dolore omnis voluptatem numquam\net omnis occaecati quod ullam at\nvoluptatem error expedita pariatur\nnihil sint nostrum voluptatem reiciendis et"
+  },
+  {
+    "postId": 1,
+    "id": 3,
+    "name": "odio adipisci rerum aut animi",
+    "email": "Nikita@garfield.biz",
+    "body": "quia molestiae reprehenderit quasi aspernatur\naut expedita occaecati aliquam eveniet laudantium\nomnis quibusdam delectus saepe quia accusamus maiores nam est\ncum et ducimus et vero voluptates excepturi deleniti ratione"
+  },
+  {
+    "postId": 1,
+    "id": 4,
+    "name": "alias odio sit",
+    "email": "Lew@alysha.tv",
+    "body": "non et atque\noccaecati deserunt quas accusantium unde odit nobis qui voluptatem\nquia voluptas consequuntur itaque dolor\net qui rerum deleniti ut occaecati"
+  },
+  {
+    "postId": 1,
+    "id": 5,
+    "name": "vero eaque aliquid doloribus et culpa",
+    "email": "Hayden@althea.biz",
+    "body": "harum non quasi et ratione\ntempore iure ex voluptates in ratione\nharum architecto fugit inventore cupiditate\nvoluptates magni quo et"
+  }
+]
